c950599095964ed5bdfcf330c985adc68fd7cd1d
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-04-05 20:21:35 -0500

    basic authorization request working from client side

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
+// 816806001599-570v0vdbknp1u364n6b1a1goni7mlt91.apps.googleusercontent.com // client ID
+// nTBGIk3szlxbCO8XeqnGYO33 // client secret
 // found in the LICENSE file.
 
 /**
@@ -89,30 +91,11 @@ function getImageUrl(searchTerm, callback, errorCallback) {
 }
 
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
 
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+$(window).ready(function() {
+    $('#authorize-button').on('click', function(e) {
+        handleAuthClick(e);
     });
-  });
 });
